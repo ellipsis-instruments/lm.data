2b3759dc223ff6531bada69d5ad87d86eb95befb
lumiica developer <devel@lumiica.com>
2018-09-12 09:40:29 -0400

    added histrets for legacy compatability

---
+++ R/_data_.R
@@ -0,0 +1,18 @@
+#' Historic monthly index values for multiple USD asset classes
+#'
+#' A dataset containing monthly USD multi-asset index values from
+#' December 1993 - August 2018
+#'
+#' @format A data frame with 297 rows and 25 variables
+#' @source Bloomberg
+"us_multi_hist"
+
+#' Historic monthly index values for multiple USD asset classes
+#'
+#' A dataset containing monthly USD multi-asset index values from
+#' December 1993 - August 2018
+#' * SAME AS us_multi_hist, for legacy code purposes
+#'
+#' @format A data frame with 297 rows and 25 variables
+#' @source Bloomberg
+"histrets"
--- R/data.R
+++
@@ -1,9 +0,0 @@
-#' Historic monthly index values for multiple USD asset classes
-#'
-#' A dataset containing monthly USD multi-asset index values from
-#' December 1993 - August 2018
-#'
-#' @format A data frame with 297 rows and 25 variables
-#' @source Bloomberg
-"us_multi_hist"
-
