04b61de8361f4f96540645baeb2d184bd250856f
lumiica developer <devel@lumiica.com>
2018-09-12 13:28:15 -0400

    consolidated data to hist_navs, hist_rets, hist_sims

--- R/_data_.R
+++ R/_data_.R
@@ -5,14 +5,22 @@
 #'
 #' @format A data frame with 297 rows and 25 variables
 #' @source Bloomberg
-"us_multi_hist"
+"hist_navs"
 
-#' Historic monthly index values for multiple USD asset classes
+#' Historic monthly returns for multiple USD asset classes
 #'
-#' A dataset containing monthly USD multi-asset index values from
+#' A dataset containing monthly USD multi-asset returns from
 #' December 1993 - August 2018
-#' * SAME AS us_multi_hist, for legacy code purposes
 #'
-#' @format A data frame with 297 rows and 25 variables
-#' @source Bloomberg
-"histrets"
+#' @format A data frame with 296 rows and 25 variables
+#' @source Bloomberg, lumiica
+"hist_rets"
+
+#' Simulated annual returns for multiple USD asset classes
+#'
+#' A dataset containing 1000 simulated annual USD multi-asset returns
+#' based on monthly data from December 1993 - August 2018
+#'
+#' @format A data frame with 1000 rows and 25 variables
+#' @source Bloomberg, lumiica
+"hist_sims"
